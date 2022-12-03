429a4805eb34d5646cce4dbeb586a9d1516e2bd0
ChristophGra <christoph.grarock@gmail.com>
2022-12-03 03:53:21 +0100

    2015 day 1

---
+++ AoC/AoC2015/day01/part1.fsx
@@ -0,0 +1,8 @@
+open System.IO
+
+let input =
+    (File.ReadAllText "C:\Users\Chris\Documents\AoC\AoC\AoC2015\day01\Input.txt").ToCharArray()
+
+let floor =
+    Array.fold (fun curr x -> match x with '(' -> curr + 1 | ')' -> curr - 1) 0 input
+    
---
+++ AoC/AoC2015/day01/part2.fsx
@@ -0,0 +1,9 @@
+open System.IO
+
+let input =
+    (File.ReadAllText "C:\Users\Chris\Documents\AoC\AoC\AoC2015\day01\Input.txt").ToCharArray()
+
+let floor =
+    Array.scan (fun (floor,count) x -> match x with '(' -> floor + 1, count + 1 | ')' -> floor - 1, count + 1) (0,0) input
+    |> Array.find (fst >> (>) 0)
+    
